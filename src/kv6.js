f5bfaf336677e608c6b146e15d77f9363d50f1b2
Emil Haugberg van Veen <emilveen@gmail.com>
2019-06-06 13:33:30 +0200

    Working post requests. #14

--- src/kv6.js
+++ src/kv6.js
@@ -1,6 +1,7 @@
-var xmlParser = require("xml2json");
-var zlib = require("zlib");
-var zmq = require('zeromq');
+const xmlParser = require("xml2json");
+const zlib = require("zlib");
+const zmq = require('zeromq');
+const request = require('request')
 
 var socket = zmq.socket('sub');
 var endpoint = 'tcp://pubsub.besteffort.ndovloket.nl:7658';
@@ -8,7 +9,8 @@ var endpoint = 'tcp://pubsub.besteffort.ndovloket.nl:7658';
 const LINE_NUMBER = "22"
 const TYPE = "ARRIVAL"
 
-var createObj = (obj) => {
+/* Create punctuality metric for given object. */
+var createMetric = (obj) => {
   if (!obj || !obj[TYPE]) { return null; }
 
   var retobj = obj[TYPE];
@@ -26,6 +28,7 @@ var createObj = (obj) => {
   }
 }
 
+/* Check if object is a list. */
 var isList = (obj) => {
   try {
     return obj.length >= 1
@@ -34,6 +37,7 @@ var isList = (obj) => {
   }
 }
 
+/* Filter arrival data for a specific type and line number. */
 var filterArrivals = (type, lineNum) => (obj) => {
   var t = Object.keys(obj)[0];
   return t == type && obj[type]["lineplanningnumber"] == lineNum
@@ -50,11 +54,33 @@ var parseMessage = (message) => {
   }
 
   if (isList(pos_info)) {
-    return pos_info.filter(filterArrivals(TYPE, LINE_NUMBER)).map(createObj).filter(x => x != null);
+    return pos_info.filter(filterArrivals(TYPE, LINE_NUMBER)).map(createMetric).filter(x => x != null);
   } else {
-    var ret = filterArrivals(TYPE, LINE_NUMBER) ? createObj(pos_info) : null;
+    var ret = filterArrivals(TYPE, LINE_NUMBER) ? createMetric(pos_info) : null;
     return ret ? [ret] : [];
   }
+}
+
+/* Generate options for post request. */
+var options = data => {
+  return {
+    uri: 'http://18.216.203.6:5000',
+    method: 'POST',
+    json: data
+  }
+};
+
+/* Send post request to prometheus db. */
+var postReq = (data) => {
+  request(options(data), JSON.stringify(data),
+    (error, res, body) => {
+    if (error) {
+      console.error(error)
+      return
+    }
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(body)
+  })
 }
 
 /* Listen to messages from ndovloket and parse the data. */
@@ -71,8 +97,8 @@ socket.on('message', (topic, msg) => {
         var mes = parseMessage(json);
 
         if (mes && mes.length >= 1) {
-          // TODO: Post request database
           console.log(mes)
+          postReq(mes);
         }
     });
 })
