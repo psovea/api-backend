f8609a27c7d5e88c539e207356d1f3408c26b1e6
Emil Haugberg van Veen <emilveen@gmail.com>
2019-06-07 22:00:30 +0200

    Cleanup part 3. #35

--- src/static/get-static-data.js
+++ src/static/get-static-data.js
@@ -7,14 +7,13 @@ const fs = require('fs')
 const fetch = require('node-fetch')
 const R = require('ramda')
 
-
 var getStopIDs = () => {
-  return fetch("https://v0.ovapi.nl/tpc/")
+  return fetch('https://v0.ovapi.nl/tpc/')
 }
 
 /* Request information about a single stop. */
 var requestStop = (uri) => {
-  return fetch("https://v0.ovapi.nl/tpc/" + uri)
+  return fetch('https://v0.ovapi.nl/tpc/' + uri)
     .then(d => d.json())
     .catch(e => console.log(e))
 }
@@ -24,9 +23,9 @@ var requestStop = (uri) => {
  * uris to request.
  * */
 var uris = keys => R.compose(
-    R.map(R.join(',')),
-    R.splitEvery(keys.length / 200)
-  )(keys)
+  R.map(R.join(',')),
+  R.splitEvery(keys.length / 200)
+)(keys)
 
 /* Get all stop information. */
 var stops = (uri) => {
@@ -36,12 +35,12 @@ var stops = (uri) => {
 
 /* Get all stops per line. */
 var stopsPerLine = () => {
-    return fetch("https://v0.ovapi.nl/journey/")
+  return fetch('https://v0.ovapi.nl/journey/')
     .then(data => data.json())
     /* We only need the first and the third id from the key. */
     .then(R.compose(
       JSON.parse,
-      R.replace(/([A-Z]+)_\d+_([A-Za-z0-9]+)_\d+_\d+/g, "$1_$2"),
+      R.replace(/([A-Z]+)_\d+_([A-Za-z0-9]+)_\d+_\d+/g, '$1_$2'),
       R.toString)
     )
 }
@@ -52,97 +51,97 @@ var getRoutes = (timeStops, dataLines) => {
   var keys = R.keys(timeStops)
 
   keys.forEach(k => {
-    var e = timeStops[k];
-    var passes = R.keys(e["Passes"]);
+    var e = timeStops[k]
+    var passes = R.keys(e['Passes'])
 
     passes.forEach(p => {
       /* Extract all needed information. */
-      var obj = e["Passes"][p];
-      var operatorCode = obj["OperatorCode"];
-      var lineCode = obj["LinePlanningNumber"];
-      var transportType = obj["TransportType"];
-      var stopCode = obj["TimingPointCode"];
-      var orderNumber = obj["UserStopOrderNumber"];
-      var newP = p.replace(/([A-Z]+)_\d+_([A-Za-z0-9]+)_\d+_\d+/g, "$1_$2");
-      var numStops = dataLines[newP];
-      var direction = obj["LineDirection"];
-      var destName = obj["DestinationName50"];
-      var lineName = obj["LineName"];
-      var publicName = obj["LinePublicNumber"];
+      var obj = e['Passes'][p]
+      var operatorCode = obj['OperatorCode']
+      var lineCode = obj['LinePlanningNumber']
+      var transportType = obj['TransportType']
+      var stopCode = obj['TimingPointCode']
+      var orderNumber = obj['UserStopOrderNumber']
+      var newP = p.replace(/([A-Z]+)_\d+_([A-Za-z0-9]+)_\d+_\d+/g, '$1_$2')
+      var numStops = dataLines[newP]
+      var direction = obj['LineDirection']
+      var destName = obj['DestinationName50']
+      var lineName = obj['LineName']
+      var publicName = obj['LinePublicNumber']
 
       /* Check if this operator code has already been added to the object. */
       if (!routes[operatorCode]) {
-          routes[operatorCode] = {}
+        routes[operatorCode] = {}
       }
 
       /* Check if this line code has already been added to the object. */
       if (!routes[operatorCode][lineCode]) {
-          routes[operatorCode][lineCode] = {
-              "lineName" : lineName,
-              "destinationName" : destName,
-              "direction" : direction,
-              "lineNumberName" : publicName,
-              "transportType" : transportType,
-              "totalStops" : numStops ? numStops : -1,
-              "stops" : []
-          }
+        routes[operatorCode][lineCode] = {
+          'lineName': lineName,
+          'destinationName': destName,
+          'direction': direction,
+          'lineNumberName': publicName,
+          'transportType': transportType,
+          'totalStops': numStops || -1,
+          'stops': []
+        }
       }
 
-      routes[operatorCode][lineCode]["stops"].push({
-          stopCode: stopCode,
-          orderNumber: orderNumber
+      routes[operatorCode][lineCode]['stops'].push({
+        stopCode: stopCode,
+        orderNumber: orderNumber
       })
 
       /* Make sure that all stops are unique. */
-      routes[operatorCode][lineCode]["stops"] = R.uniq(
-        routes[operatorCode][lineCode]["stops"]
+      routes[operatorCode][lineCode]['stops'] = R.uniq(
+        routes[operatorCode][lineCode]['stops']
       )
     })
   })
 
-  fs.writeFileSync("data/route_data1.json", JSON.stringify(routes))
+  fs.writeFileSync('data/route_data1.json', JSON.stringify(routes))
 }
 
 /* Create stop object, which contains information such as lat, lon and name. */
 var getStops = (timeStops) => {
-    var keys = Object.keys(timeStops)
-    var stops = keys.map(key => {
-      var stop = timeStops[key]
+  var keys = Object.keys(timeStops)
+  var stops = keys.map(key => {
+    var stop = timeStops[key]
 
-      return {
-        [stop.Stop.TimingPointCode]: {
-          lat: stop.Stop.Latitude,
-          lon: stop.Stop.Longitude,
-          name: stop.Stop.TimingPointName,
-          town: stop.Stop.TimingPointTown,
-          areaCode: stop.Stop.StopAreaCode,
-          accessibility: {
-            wheelchair: ('ACCESSIBLE') ? true : false,
-            visual: ('ACCESSIBLE') ? true : false
-          }
+    return {
+      [stop.Stop.TimingPointCode]: {
+        lat: stop.Stop.Latitude,
+        lon: stop.Stop.Longitude,
+        name: stop.Stop.TimingPointName,
+        town: stop.Stop.TimingPointTown,
+        areaCode: stop.Stop.StopAreaCode,
+        accessibility: {
+          wheelchair: !!('ACCESSIBLE'),
+          visual: !!('ACCESSIBLE')
         }
       }
-    })
+    }
+  })
 
-    fs.writeFileSync('data/all_stops.json', JSON.stringify(stops));
+  fs.writeFileSync('data/all_stops.json', JSON.stringify(stops))
 }
 
 var main = () => {
   /* Retrieve the id's for all stops and retrieve information regarding them.
    * */
   getStopIDs()
-  .then(d => d.json())
-  .then(keys => {
-    Promise.all([stopsPerLine(), stops(uris(R.keys(keys)))])
-      .then(d => {
-        var timeStops = d[1]
-        var dataLines = d[0]
-        getStops(timeStops)
-        getRoutes(timeStops, dataLines)
-      })
-      .catch(e => console.log(e))
-  })
-  .catch(e => console.log(e))
+    .then(d => d.json())
+    .then(keys => {
+      Promise.all([stopsPerLine(), stops(uris(R.keys(keys)))])
+        .then(d => {
+          var timeStops = d[1]
+          var dataLines = d[0]
+          getStops(timeStops)
+          getRoutes(timeStops, dataLines)
+        })
+        .catch(e => console.log(e))
+    })
+    .catch(e => console.log(e))
 }
 
-main();
+main()
